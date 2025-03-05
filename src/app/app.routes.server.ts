28a80b194f9a35879d55bb4ca4d3518119d64dbe
Salma-Yahya24 <salmayahya1812@gmail.com>
2025-03-05 21:51:56 +0200

    edit2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,7 +5,14 @@ export const serverRoutes: ServerRoute[] = [
     path: 'allorders/:userId',
     renderMode: RenderMode.Server
   },
-  
+  {
+    path: 'shipping-address/:cartId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'productdetails/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
