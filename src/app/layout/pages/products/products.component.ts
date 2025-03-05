76251b2ec1b94b814caf292acbe3f30c596ca174
Salma-Yahya24 <salmayahya1812@gmail.com>
2025-03-06 00:31:20 +0200

    edit product

--- src/app/layout/pages/products/products.component.ts
+++ src/app/layout/pages/products/products.component.ts
@@ -13,46 +13,50 @@ import { CartService } from '../../../shared/services/cart/cart.service';
   selector: 'app-products',
   imports: [CurrencyPipe, UpperCasePipe, SearchPipe, RouterLink, FormsModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit {
-    productList!: product[];
-    userWord: string = '';
+  productList!: product[];
+  userWord: string = '';
+  searchBy: string = 'title';
+  constructor(
+    private _ProductService: ProductService,
+    private _CartService: CartService,
+    private _WishListService: WishListService,
+    private _ToastrService: ToastrService
+  ) {}
 
-constructor(private _ProductService:ProductService, private _CartService:CartService, private _WishListService:WishListService, private _ToastrService: ToastrService){}
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+  getAllProduct() {
+    this._ProductService.getAllProducts().subscribe({
+      next: (res) => {
+        this.productList = res.data;
+      },
+    });
+  }
+  addProductToCart(productId: string) {
+    this._CartService.addProductToCart(productId).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._CartService.cartNo.next(res.numOfCartItems);
+        this._ToastrService.success(res.message, 'Success', {
+          timeOut: 2000,
+          progressBar: true,
+        });
+      },
+    });
+  }
 
-ngOnInit(): void {
-this.getAllProduct();
-}
-getAllProduct() {
-  this._ProductService.getAllProducts().subscribe({
-    next: (res) => {
-      this.productList = res.data;
-    },
-  });
-}
-addProductToCart(productId: string) {
-  this._CartService.addProductToCart(productId).subscribe({
-    next: (res) => {
-      console.log(res);
-      this._CartService.cartNo.next(res.numOfCartItems);
-      this._ToastrService.success(res.message, 'Success', {
-        timeOut: 2000,
-        progressBar: true,
-      });
-    },
-  });
-}
-
-addProductToWishList(productId: string) {
-  this._WishListService.addProductToWishList(productId).subscribe({
-    next: (res) => {
-      
-      this._ToastrService.success(res.message, 'Success', {
-        timeOut: 2000,
-        progressBar: true,
-      });
-    },
-  })
-}
+  addProductToWishList(productId: string) {
+    this._WishListService.addProductToWishList(productId).subscribe({
+      next: (res) => {
+        this._ToastrService.success(res.message, 'Success', {
+          timeOut: 2000,
+          progressBar: true,
+        });
+      },
+    });
+  }
 }
