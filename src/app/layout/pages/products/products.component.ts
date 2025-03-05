a59ce098c487bf8ffc0db0de0747f6c7d5239bc9
Salma-Yahya24 <salmayahya1812@gmail.com>
2025-03-06 00:44:00 +0200

    edit search

--- src/app/layout/pages/products/products.component.ts
+++ src/app/layout/pages/products/products.component.ts
@@ -19,6 +19,7 @@ export class ProductsComponent implements OnInit {
   productList!: product[];
   userWord: string = '';
   searchBy: string = 'title';
+  
   constructor(
     private _ProductService: ProductService,
     private _CartService: CartService,
