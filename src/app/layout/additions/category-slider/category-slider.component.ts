308283211d8d23161706a76f873ebf6a88c28016
Salma-Yahya24 <salmayahya1812@gmail.com>
2025-03-06 00:04:40 +0200

    category slider

--- src/app/layout/additions/category-slider/category-slider.component.ts
+++ src/app/layout/additions/category-slider/category-slider.component.ts
@@ -28,7 +28,7 @@ export class CategorySliderComponent implements OnInit {
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
-    touchDrag: false,
+    touchDrag: true,
     pullDrag: false,
     dots: false,
     navSpeed: 700,
